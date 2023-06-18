c76f90a24f92e604b07337779352d6554fa7a744
davudxD <carovacdavud6@gmail.com>
2023-06-18 21:50:01 +0200

    youtube clone app - making cards

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Typography, Card, CardContent, CardMedia} from '@mui/material';
-import {CheckCircle} from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
 
-import {demoThumbnailUrl, demoVideoUrl, demoChannelTitle, demoChannelUrl} from '../utils/constants';
+import { demoThumbnailUrl, demoVideoUrl, demoChannelTitle, demoChannelUrl, demoVideoTitle } from '../utils/constants';
 
-const VideoCard = ({video: {id: {videoId}, snippet}}) => {
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   // console.log(videoId, snippet);
   // console.log(snippet?.thumbnails?.high?.url)
   return (
-    <Card sx={{ border: "none"}}>
+    <Card sx={{ border: "none" }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-      <CardMedia
-       image={snippet?.thumbnails?.high?.url}
-       alt={snippet?.title}
-       sx={{width: 358, height:180, border: "none"}}
-       />
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180, border: "none" }}
+        />
       </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#fff">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            <CheckCircle sx={{fontSize: 12, color: "gray", ml: "5px"}}/>
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   )
 }
