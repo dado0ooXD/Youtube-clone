6d0fe43aa3fce4c3b815815af5c6b40919a66b7c
davudxD <carovacdavud6@gmail.com>
2023-06-21 15:54:45 +0200

    youtube clone app - styles update

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -17,7 +17,7 @@ const Feed = () => {
   }, [selectedCategory])
 
   return (
-    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" }, background: "#0f0f0f" }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: {sx:0, md: 2} }}>
         <SideBar selectedCategory = {selectedCategory} setSelectedCategory = {setSelectedCategory}/>
         <Typography className="copyright"
