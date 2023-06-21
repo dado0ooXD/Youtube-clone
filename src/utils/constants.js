aa5ff02168e6085b7e3b83672e50b2c48bfd3d0f
davudxD <carovacdavud6@gmail.com>
2023-06-21 15:48:14 +0200

    youtube clone app - icons update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,8 @@ import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
 import SchoolIcon from '@mui/icons-material/School';
-import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
-import CheckroomIcon from '@mui/icons-material/Checkroom';
+import PublicIcon from '@mui/icons-material/Public';
+import SportsBasketballIcon from '@mui/icons-material/SportsBasketball';
 import GraphicEqIcon from '@mui/icons-material/GraphicEq';
 import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
@@ -15,11 +15,7 @@ import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
-    { name: 'New', icon: <HomeIcon />, },
-    { name: 'JS Mastery', icon: <CodeIcon />, },
-    { name: 'Coding', icon: <CodeIcon />, },
     { name: 'ReactJS', icon: <CodeIcon />, },
-    { name: 'NextJS', icon: <CodeIcon />, },
     { name: 'Football', icon: <SportsSoccerIcon /> },
     { name: 'Education', icon: <SchoolIcon />, },
     { name: 'Podcast', icon: <GraphicEqIcon />, },
@@ -27,8 +23,8 @@ export const categories = [
     { name: 'Gaming', icon: <SportsEsportsIcon />, },
     { name: 'Live', icon: <LiveTvIcon />, },
     { name: 'Sport', icon: <FitnessCenterIcon />, },
-    { name: 'Fashion', icon: <CheckroomIcon />, },
-    { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+    { name: 'Basketball', icon: <SportsBasketballIcon />, },
+    { name: 'Trending', icon: <PublicIcon />, },
     { name: 'Comedy', icon: <TheaterComedyIcon />, },
     { name: 'Gym', icon: <FitnessCenterIcon />, },
     { name: 'Crypto', icon: <DeveloperModeIcon />, },
